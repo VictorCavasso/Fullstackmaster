6030ca8e7972c39230034deff5fa7e02e19bd826
VictorCavasso <75066936+VictorCavasso@users.noreply.github.com>
2021-08-06 00:48:38 -0300

    CV Project - 1.00

---
+++ cavasso/index.js
@@ -0,0 +1,27 @@
+const express = require('express')          
+const app = express()
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/', (request, response) => {
+    response.render('home')
+})
+
+app.get('/grv', (request, response) => {
+    response.render('grv')
+})
+
+app.get('/tur', (request, response) => {
+    response.render('tur')
+})
+
+// https 443
+// http 80
+app.listen(3000, (err) => {
+    if(err){
+        console.log('Não foi possivel inciar o servidor!')
+    }else{
+        console.log('Servidor rodando...')
+    }
+})
